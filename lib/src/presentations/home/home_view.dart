a7199137795cd6ef9542c1cb5add71765efe033f
nell <devstory.co.kr@gmail.com>
2025-08-24 01:37:15 +0900

    Feat: add VideoViewer.

--- lib/src/presentations/home/home_view.dart
+++ lib/src/presentations/home/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:estopia/core/base/base_view.dart';
 import 'package:estopia/core/lang/generated/l10n.dart';
+import 'package:estopia/core/themes/components/video/video_viewer.dart';
 import 'package:estopia/src/presentations/home/home_view_model.dart';
 import 'package:estopia/src/presentations/home/home_view_state.dart';
 import 'package:flutter/material.dart';
@@ -16,6 +17,10 @@ class HomeView extends StatelessWidget {
       builder: (context, viewModel, state) => Scaffold(
         appBar: AppBar(
           title: Text(S.current.appName),
+        ),
+        body: AspectRatio(
+          aspectRatio: 16 / 9,
+          child: VideoViewer(controller: viewModel.videoPlayerController),
         ),
       ),
     );
--- lib/src/presentations/home/home_view_model.dart
+++ lib/src/presentations/home/home_view_model.dart
@@ -1,6 +1,12 @@
 import 'package:estopia/core/base/base_view_model.dart';
 import 'package:estopia/src/presentations/home/home_view_state.dart';
+import 'package:video_player/video_player.dart';
 
 class HomeViewModel extends BaseViewModel<HomeViewState> {
   HomeViewModel(super.initialState);
+
+  final VideoPlayerController videoPlayerController =
+      VideoPlayerController.asset(
+        "assets/videos/sample.mp4",
+      );
 }
